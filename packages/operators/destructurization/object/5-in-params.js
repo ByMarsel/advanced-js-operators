6aa9cc9dc17e962b1d759a87c400bc0c368f1b67
Марсель Ахметшин <marsel@marsel.local>
2024-01-29 20:21:29 +0400

    fixes again

--- packages/operators/destructurization/object/5-in-params.js
+++ packages/operators/destructurization/object/5-in-params.js
@@ -5,12 +5,4 @@
 function greet({ name, age }) {
   console.log(`Hello, my name is ${name} and I'm ${age} years old.`);
 }
-greet({ name: "Alice", age: 30 });
-
-
-function setOptions({ width = 100, height = 100, color = "black" } = {}) {
-  console.log(`Width: ${width}, Height: ${height}, Color: ${color}`);
-}
-
-setOptions({ width: 200 }); // Выводит "Width: 200, Height: 100, Color: black"
-setOptions(); // Выводит "Width: 100, Height: 100, Color: black"
+greet({ name: "Alice", age: 30 }); // Hello, my name is Alice and I'm 30 years old.

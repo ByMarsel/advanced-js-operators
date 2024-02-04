f199d5a34043cbd44d206410ff27c1aa2eb5325b
Марсель Ахметшин <marsel@marsel.local>
2024-02-04 20:13:30 +0400

    added example without destructuring

--- packages/operators/destructurization/object/5-in-params-old.js
+++ packages/operators/destructurization/object/5-in-params-old.js
@@ -2,3 +2,16 @@
  * ? Деструктуризация объекта в параметрах функции. Без нового синтаксиса
  */
 
+function greet(person) {
+  const name = person.name;
+  const age = person.age;
+
+  console.log(`Hello, ${name}. You are ${age} years old.`);
+}
+
+console.log(
+  greet({
+    name: "John",
+    age: 30,
+  })
+); // Hello, John. You are 30 years old.
